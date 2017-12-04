dbfc315cbc01ee39597c68272ca4d240df9ab6f0
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-04 13:59:45 +0000

    hello world

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(4000, function () {
+    console.log("Example listening on port 3000!")
+});
