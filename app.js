caa2cb26eb9b7a7e1406c228234bca66b3dbb877
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-05 18:48:39 +0000

    get Token

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-if (process.argv.length < 3){
-    console.log('node' + process.argv[1] + 'filename'  );
+if (process.argv.length < 3) {
+    console.log('node' + process.argv[1] + 'filename');
     process.exit(1);
 }
 
+var request = require('request');
+var wsClient = require('websocket').client;
 var fs = require('fs');
+
+var azureClienteSecret = 'a56ac805d4664a66b5b8313b0329d6be';
+var textTranslateUrl = 'https://api.microsofttranslator.com/V2/Http.svc/Translate';
 var filename = process.argv[2];
-fs.readFile(filename,'utf8',function(err, data){
+
+var xmlRequest = '<TranslateArrayRequest> <AppId/> <From>language-code</From> <Texts>  <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">string-value</string>   <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">string-value</string> </Texts> <To>language-code</To> </TranslateArrayRequest>'
+
+request.post({
+        url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
+        headers: {
+            'Ocp-Apim-Subscription-Key': azureClienteSecret
+        },
+        method: 'POST'
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var accessToken = body;
+
+            console.log(accessToken);
+        }
+    }
+);
+
+request.post
+
+fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
-    console.log('File ' + filename);
-    console.log(data);
+  //  console.log('File ' + filename);
+    //console.log(data);
 
 });
