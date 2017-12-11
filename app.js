b556c0a996eab57061cb272aaff08b530709a54a
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-11 18:01:49 +0000

    Read JSON file and filters according selected language

--- app.js
+++ app.js
@@ -3,58 +3,25 @@ if (process.argv.length < 3) {
     process.exit(1);
 }
 
-var request = require('request');
 var fs = require('fs');
-
-var azureClienteSecret = 'a56ac805d4664a66b5b8313b0329d6be';
-// var textTranslateUrl = 'http://api.microsofttranslator.com/V2/Http.svc/Translate'
+var MsTranslator = require('mstranslator');
+var key = 'a56ac805d4664a66b5b8313b0329d6be';
 var filename = process.argv[2];
-var text = '';
-var accessToken;
-
-request.post({
-        url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
-        headers: {
-            'Ocp-Apim-Subscription-Key': azureClienteSecret
-        },
-        method: 'POST'
-    },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            accessToken = body;
-        }
-    }
-);
-
-fs.readFile(filename, 'utf8', function (err, data) {
-    if (err) throw err;
-    text = data;
-});
-
-setTimeout(function () {
-    var to = 'en';
-    var from = 'pt';
-    var textTranslateUrl = "http://api.microsofttranslator.com/v2/http.svc/translate?text=" + text + "&from=" + from + "&to=" + to;
 
 
-    var headers = {
-        Authorization: 'Bearer ' + accessToken
-    }
-    var options = {
-        url: textTranslateUrl,
-        method: 'GET',
-        headers: headers,
-        // text: 'water',
-        // to: 'pt',
-        // from: 'en'   
-    }
+var rawdata = fs.readFileSync(filename);
+var data = JSON.parse(rawdata);
+
+for (i in data){
+    if(data[i].lang == 'en')
+        console.log(data[i].entries);
+}
 
 
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log(error);
-            console.log(response);
-        }
-        console.log(body)
-    });
-}, 3000);
+
+
+// var client = new MsTranslator({
+//     api_key = 'a56ac805d4664a66b5b8313b0329d6be'
+// }, true);
+
+
