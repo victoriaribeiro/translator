1c7525ed4f6767e68f8d5e0ffa6c6591ae76ab01
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-12 17:37:12 +0000

    Revert "Separa objetos para tradução"
    This reverts commit d0db0a2cc9683bfc562c333ea8a715d40442bec9.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var key = 'a56ac805d4664a66b5b8313b0329d6be';
 var filename = process.argv[2];
 var toTranslate = [];
 var fromLang = 'en';
-var toLang = 'pt';
+var toLang = 'de';
 var total = 0;
 
 
@@ -20,47 +20,34 @@ var client = new MsTranslator({
     api_key: key
 }, true);
 
-var params = {
-    texts: toTranslate,
-    from: fromLang,
-    to: toLang,
-}
-
 
 for (i in data) {
     if (data[i].lang == fromLang) {
         for (j in data[i].entries) {
             total += data[i].entries[j].value.length;
             toTranslate.push(data[i].entries[j].value);
-
+        
             //console.log(data[i].entries[j].value);
         }
-        params.texts = toTranslate;
-        console.log(toTranslate);
-        client.translateArray(params, function (err, response) {
-            console.log(toTranslate);
-            
-            console.log(response)
-            //fs.appendFileSync('/home/victoria/response.json', JSON.stringify(response));
-            
-        });
-
-         //fs.writeFileSync('/home/victoria/response.json', JSON.stringify(response));
     }
-    toTranslate = []
+
     //console.log(data[i].entries);
 }
 console.log(total);
 
 
+var params = {
+    texts: toTranslate,
+    from: fromLang,
+    to: toLang,
+}
 
+client.translateArray(params, function (err, response) {
+    if (err) throw err;
 
-// client.translateArray(params, function (err, response) {
-//     if (err) throw err;
+    fs.writeFileSync('/home/victoria/response.json', JSON.stringify(response));
 
-//     fs.writeFileSync('/home/victoria/response.json', JSON.stringify(response));
-
-//     for (i in response) {
-//         console.log(response[i].TranslatedText);
-//     }
-// });
+    for (i in response) {
+        console.log(response[i].TranslatedText);
+    }
+});
