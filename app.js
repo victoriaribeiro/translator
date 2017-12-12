8d4d4a11f0300493a4d9ef4fcd6b0b719ad5286a
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-12 16:49:56 +0000

    Reads JSON file, filters according to selected language and receives translation for multiple sentences in JSON format

--- app.js
+++ app.js
@@ -7,21 +7,45 @@ var fs = require('fs');
 var MsTranslator = require('mstranslator');
 var key = 'a56ac805d4664a66b5b8313b0329d6be';
 var filename = process.argv[2];
+var toTranslate = [];
+var fromLang = 'en';
+var toLang = 'de';
+var total = 0;
 
 
 var rawdata = fs.readFileSync(filename);
 var data = JSON.parse(rawdata);
 
-for (i in data){
-    if(data[i].lang == 'en')
-        console.log(data[i].entries);
+for (i in data) {
+    if (data[i].lang == fromLang) {
+        for (j in data[i].entries) {
+            total += data[i].entries[j].value.length;
+            toTranslate.push(data[i].entries[j].value);
+
+            //console.log(data[i].entries[j].value);
+        }
+    }
+
+    //console.log(data[i].entries);
+}
+console.log(total);
+
+var client = new MsTranslator({
+    api_key: key
+}, true);
+
+var params = {
+    texts: toTranslate,
+    from: fromLang,
+    to: toLang,
 }
 
+client.translateArray(params, function (err, response) {
+    if (err) throw err;
 
+    fs.writeFileSync('/home/victoria/response.json', JSON.stringify(response));
 
-
-// var client = new MsTranslator({
-//     api_key = 'a56ac805d4664a66b5b8313b0329d6be'
-// }, true);
-
-
+    for (i in response) {
+        console.log(response[i].TranslatedText);
+    }
+});
