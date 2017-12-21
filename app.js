819aa448c5c6a2538f65bd102798c36f9168da0b
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-21 18:21:55 +0000

    translates to multiples languages now

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ if (process.argv.length < 5) {
     process.exit(1);
 }
 
-//async = require('async');
 var fs = require('fs');
 var MsTranslator = require('mstranslator');
 var key = 'a56ac805d4664a66b5b8313b0329d6be';
@@ -27,88 +26,29 @@ if (process.argv.length >= 5) {
         toLangs.push(process.argv[i]);
     }
 }
-// else{
-//     params.to = process.argv[4];
-// }
-
-var total = 0;
-
-
-
 
 var rawdata = fs.readFileSync(filename);
 var data = JSON.parse(rawdata);
-fs.appendFileSync('/home/victoria/entries.json', JSON.stringify(data));
 
-
-// function getTexts(index) {
-//     // for (k = 0; k < toLangs.length;) {
-//     while (index < toLangs.length) {
-//         index++;
-//         getTexts(index);
-//         console.log(index);
-//         params.to = toLangs[index];
-//         for (i = 0; i < data.length; i++) {
-//             if (data[i].lang == fromLang) {
-//                 for (j in data[i].entries) {
-//                     total += data[i].entries[j].value.length;
-//                     toTranslate.push(data[i].entries[j].value);
-//                     //console.log(data[i].entries[j].value)
-//                 }
-//             }
-//         }
-//         params.texts = toTranslate;
-//         console.log(params.to)
-
-//         client.translateArray(params, function (err, data) {
-//             console.log(err);
-//             console.log(data)
-//             // index++;
-//         })
-//     }
-
-//     // }
-// }
-
-
-
-
-
-entries = 0;
-for (k = 0; k < toLangs.length; k++) {
-    total = 0;
-    entries  = 0;
-    params.to = toLangs[k];
-    console.log(params.to)
-    for (i = 0; i < data.length; i++) {
-        if (data[i].lang == fromLang) {
-            entries++;
-            for (j in data[i].entries) {               
-                total += data[i].entries[j].value.length;
-                toTranslate.push(data[i].entries[j].value);
-                // console.log(data[i].entries[j].value)
-            }
+for (i = 0; i < data.length; i++) {
+    if (data[i].lang == fromLang) {
+        for (j in data[i].entries) {
+            toTranslate.push(data[i].entries[j].value);
         }
     }
-    params.texts = toTranslate;
-    debugger;
-    client.translateArray(params, function (err, response) {
-        debugger;
-        if (err) throw err
-        
-        //fs.appendFileSync('/home/victoria/response.json', JSON.stringify(response));
-        //console.log(data);
-    })
 }
+params.texts = toTranslate;
 
-console.log(entries);
+toLangs.forEach(function(lang){
+    let translation = Object.assign({
+        to: lang
+    }, {
+        texts: params.texts,
+        from: params.from
+    });
 
-// client.translateArray(params, function (err, response) {
-//     if (err) throw err;
-
-//     fs.writeFileSync('/home/victoria/response.json', JSON.stringify(response));
-
-//     // for (i in response) {
-//     //     console.log(response[i].TranslatedText);
-//     // }
-// });
+    client.translateArray(translation, (err, result) => {
+        console.log(err);
+        console.log(result);
+    });
+})
