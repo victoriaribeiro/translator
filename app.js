433dabe2c6f957c87bd1446c9366556d980fe8cb
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-21 22:33:31 +0000

    npm install eslint npm i -S eslint eslint-config-airbnb-base eslint-plugin-import

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
 if (process.argv.length < 5) {
-    console.log('node ' + process.argv[1] + ' filename fromLang toLang');
-    process.exit(1);
+  console.log('node'+ process.argv[1]+'filename fromLang toLang');  
+  process.exit(1);
 }
 
-var MsTranslator = require('mstranslator');
-var key = 'a56ac805d4664a66b5b8313b0329d6be';
-var filename = process.argv[2];
-var toTranslate = [];
-var fromLang = process.argv[3];
-var toLangs = [];
+const MsTranslator = require('mstranslator');
 
-var client = new MsTranslator({
-    api_key: key
+const key = 'a56ac805d4664a66b5b8313b0329d6be';
+const filename = process.argv[2];
+const toTranslate = [];
+const fromLang = process.argv[3];
+const toLangs = [];
+
+const client = new MsTranslator({
+  api_key: key,
 }, true);
 
-var params = {
-    texts: '',
-    from: fromLang,
-    to: '',
-}
+const params = {
+  texts: '',
+  from: fromLang,
+  to: '',
+};
 const fileData = require(filename);
 
 
 if (process.argv.length >= 5) {
-    for (i = 4; i < process.argv.length; i++) {
-        toLangs.push(process.argv[i]);
-    }
+  for (let i = 4; i < process.argv.length; i += 1) {
+    toLangs.push(process.argv[i]);
+  }
 }
 
-
-for (i = 0; i < fileData.length; i++) {
-    if (fileData[i].lang == fromLang) {
-        for (j in fileData[i].entries) {
-            toTranslate.push(fileData[i].entries[j].value);
-        }
+fileData.forEach((dataEntry) => {
+  if (dataEntry.lang === fromLang) {
+    for (i in dataEntry.entries) {
+      toTranslate.push(dataEntry.entries[i].value);
     }
-}
+  }
+});
+
 params.texts = toTranslate;
 
-toLangs.forEach(function(lang){
-    let translation = Object.assign({
-        to: lang
-    }, {
-        texts: params.texts,
-        from: params.from
-    });
+toLangs.forEach((lang) => {
+  const translation = Object.assign({
+    to: lang,
+  }, {
+    texts: params.texts,
+    from: params.from,
+  });
 
-    client.translateArray(translation, (err, result) => {
-        console.log(err);
-        console.log(result);
-    });
-})
+  client.translateArray(translation, (err, result) => {
+    console.log(err); 
+    console.log(result); 
+  });
+});
