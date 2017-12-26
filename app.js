c4a558ac0785652621dd279120bc4b9c3deaf616
Victória Ribeiro Rodrigues <victoria.ribeirorodrigues@gmail.com>
2017-12-26 18:42:53 +0000

    objectID not working yet

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ if (process.argv.length < 5) {
   process.exit(1);
 }
 
-const MsTranslator = require('mstranslator');
 
+const MsTranslator = require('mstranslator');
+const mongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017'
 const key = 'a56ac805d4664a66b5b8313b0329d6be';
 const filename = process.argv[2];
 const toTranslate = [];
 const fromLang = process.argv[3];
 const toLangs = [];
+
+
+
+
+
 
 const client = new MsTranslator({
   api_key: key,
@@ -21,6 +28,9 @@ const params = {
   to: '',
 };
 const fileData = require(filename);
+//var dbase = null;
+
+
 
 
 if (process.argv.length >= 5) {
@@ -29,26 +39,77 @@ if (process.argv.length >= 5) {
   }
 }
 
-fileData.forEach((dataEntry) => {
-  if (dataEntry.lang === fromLang) {
-    for (i in dataEntry.entries) {
-      toTranslate.push(dataEntry.entries[i].value);
-    }
-  }
-});
+mongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  dbase = db.db('translator');
+ console.log('teste')
+  dbase.createCollection('translations', (err, collection) => {
+    console.log('Collection created');
 
-params.texts = toTranslate;
+
+    collection.find({"lang": "en"}).forEach((doc) => {  
+      let toTranslate = []
+      doc.entries.forEach((entries)=>{
+        toTranslate.push(entries.value)
+
+      })
+
 
 toLangs.forEach((lang) => {
   const translation = Object.assign({
     to: lang,
+          texts: toTranslate
   }, {
-    texts: params.texts,
+          
     from: params.from,
   });
+        
+        client.translateArray(translation, (err, result) => {
+          //debugger
+          if (err) throw err
+          // console.log(translation.texts)
+          // console.log(result);
+          result.forEach((response) => {
+            let newDoc = doc
+           // let obj = new ObjectID();
+            debugger;
+            // newDoc.lang
+            //collection.updateOne(newDoc,)
+          })
+        });
+      });
+      //console.log('oi')
+      db.close()
 
-  client.translateArray(translation, (err, result) => {
-    console.log(err);
-    console.log(result);
+    });
+    //db.close()
+    
+
   });
+  debugger;
+  //db.close();
 });
+       
+// fileData.forEach((dataEntry) => {
+//   if (dataEntry.lang === fromLang) {
+//     for (i in dataEntry.entries) {
+//       toTranslate.push(dataEntry.entries[i].value);
+//     }
+//   }
+// });
+
+//params.texts = toTranslate;
+
+// toLangs.forEach((lang) => {
+//   const translation = Object.assign({
+//     to: lang,
+//   }, {
+//     texts: params.texts,
+//     from: params.from,
+//   });
+
+//   client.translateArray(translation, (err, result) => {
+//     console.log(err);
+//     console.log(result[1]);
+//   });
+// });
